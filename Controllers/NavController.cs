1f6a4d6f8baf12f3ae881772fb4d557274253851
ochiengowino <benjaminochieng99@gmail.com>
2024-02-12 08:50:19 +0300

    changes

--- Controllers/NavController.cs
+++ Controllers/NavController.cs
@@ -17,17 +17,12 @@ namespace API.Controllers
     public class NavController : ApiController
     {
 
-        // private readonly NavContext _navcontext;
-        //private LoanApplicationsEntities1 db = new LoanApplicationsEntities1();
+       
         private LoanApplicationEntities db = new LoanApplicationEntities();
         LoanApplicationList_Service _ws = new LoanApplicationList_Service();
         LoanApplicationList _client = new LoanApplicationList();
         
-        //SqlConnection conn = new SqlConnection("Server=OCHIENGOWINO\\NAVDEMO; Database=LoanApplications; Trusted_Connection=True; MultipleActiveResultSets=True; TrustServerCertificate=True");
-        /*   public NavController(NavContext navcontext)
-           {
-               _navcontext = navcontext;
-           }*/
+      
         public NavController()
         {
            // LoanApplicationList_Service _ws = new LoanApplicationList_Service();
@@ -41,15 +36,6 @@ namespace API.Controllers
         public IHttpActionResult Get()
         {
             
-
-            //var dat = db.LoanApplications.ToList();
-            
-            //var name = client.Member_Name;
-
-            //var data = ws.Read(client.Member_Name);
-            // var filter = new LoanApplicationList_Filter();
-            //filter.Field = LoanApplicationList_Fields.Member_Name;
-            //var items = ws.ReadMultiple(null, "", 5);
 
             List<LoanApplicationList_Filter> filters = new List<LoanApplicationList_Filter>();
             LoanApplicationList_Filter filter = new LoanApplicationList_Filter
@@ -104,25 +90,6 @@ namespace API.Controllers
             var entries = db.LoanApplications.ToList();
             try
             {
-
-                /*  var loan_no = _client.Loan_No;
-                 var application_date = _client.Application_Date;
-                 var loan_prod_type = _client.Loan_Product_Type;
-                 var member_no = _client.Member_No;
-                 var member_name = _client.Member_Name;
-                 var req_amount = _client.Requested_Amount;
-                 var appr_amount = _client.Application_Date;
-                 var interest = _client.Interest;
-                 var status = _client.Status;
-                 var recID = _client.RecID;
-                 var prduct_type_name = _client.Loan_Product_Type_Name;*/
-
-
-
-                //loan_no = "LBN00096";
-                // member_name = "John Doe";
-                //application_date = (DateTime)entry.Application_Date;
-                // prduct_type_name = "Development Loan";
                 List<object> LoanApp = new List<object>();
                 if (entries != null)
                 {
@@ -145,20 +112,7 @@ namespace API.Controllers
                                 //RecID = entry.RecID,
                                 // Status = (string)entry.Status
                             };
-                           // LoanApp.Add(loanList);
-                            //loan_no = "LBN00096";
-                            // _client.Loan_No = entry.Loan_No;
-                           /* _client.Member_Name = entry.Member_Name;
-                            _client.Member_No = entry.Member_No;*/
-                           // _client.Loan_Product_Type = entry.Loan_Product_Type;
-                           // _client.Requested_Amount = (decimal)entry.Requested_Amount;
-                            /*       _client.Application_Date = (DateTime)entry.Application_Date;
-                                  _client.Loan_Product_Type = entry.Loan_Product_Type;
-                                  _client.Loan_Product_Type_Name = entry.Loan_Product_Type_Name;
-                                  _client.Requested_Amount = (decimal)entry.Requested_Amount;
-                                  _client.Approved_Amount = (decimal)entry.Approved_Amount;
-                                  _client.Member_No = "1181054";*/
-                          //  _ws.Create(ref _client);
+                      
 
                              _ws.Create(ref loanList);
                             return Ok(loanList);
@@ -166,8 +120,7 @@ namespace API.Controllers
 
 
                     }
-                    // _ws.Create(ref _client);
-                    //_ws.CreateMultiple(entries);
+                   
                     return Ok("Data sent to nav");
 
                 }
@@ -176,7 +129,6 @@ namespace API.Controllers
                     return BadRequest("No data found in the DB");
                 }
 
-                //return Ok(_client);
             }
             catch (Exception ex)
             {
